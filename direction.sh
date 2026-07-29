21dd9b9d59595ef591dc0f138871c3bf197a9c46
Narasimha Raju <Narasimha8545@gmail.com>
2026-07-29 09:52:02 +0530

    directions

--- direction.sh
+++ direction.sh
@@ -35,7 +35,7 @@ else
     else
         echo -e " $B mysql is already installed.. nothing do it $N "|tee -a $LOG_FILE
     fi
-    dnf list installed python3 -y
+    dnf list installed python3 -y &>>$LOG_FILE
     if [ $? -ne 0 ]
     then 
         echo "python3 is not installed.. going to install it"|tee -a $LOG_FILE
@@ -43,7 +43,7 @@ else
     else
         echo -e " $B python3 is already installed.. nothing do it $N "|tee -a $LOG_FILE
     fi
-    dnf list installed nginx -y
+    dnf list installed nginx -y&>>$LOG_FILE
     if [ $? -ne 0 ]
     then
         echo "nginx is not installed.. going to install it" |tee -a $LOG_FILE
